b64d76f18a0f5a308a1d9c6aeec44d831eab82d9
ANDRAS VARGA <vargaae@hotmail.com>
2025-01-30 00:46:35 +0100

    fixing bugs - restart

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
-// import "./../app/app.css";
+import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
-// import "@aws-amplify/ui-react/styles.css";
+import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(outputs);
 
