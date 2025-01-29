687da86d25e4a9a32b12e2b2de1f6118b2708e7f
ANDRAS VARGA <vargaae@hotmail.com>
2025-01-29 22:55:57 +0100

    Deploy SportDATA APP 2025 V2.1.0 to AWS

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Status from "@/components/status/Status";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "../amplify/data/resource";
 import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
+import outputs from "@/amplify_outputs.json";
 
 Amplify.configure(outputs);
 
