c572894a914cc0169d4e9d2c3ad060a124e936b3
ANDRAS VARGA <vargaae@hotmail.com>
2025-01-30 01:48:09 +0100

    fix build failure - reset to  3c5133b

---
+++ app/types.ts
@@ -0,0 +1,60 @@
+export type apiOptions = {
+  next: any;
+  headers: {
+    "X-Auth-Token": string | any;
+    "Content-Type": string | any;
+  };
+};
+
+export type matchesArea = {
+  id?: number;
+  name: string;
+};
+export type matchesCompetition = {
+  id?: number;
+  name: string;
+  code: string;
+  emblem: string;
+};
+export type matchesHomeTeam = {
+  id?: number;
+  name: string;
+  crest: string;
+};
+export type matchesAwayTeam = {
+  id?: number;
+  name: string;
+  crest: string;
+};
+export type scores = {
+  fullTime: {
+    home: number;
+    away: number;
+  };
+  halfTime?: {
+    home: number;
+    away: number;
+  };
+};
+
+export type matchesType = {
+  code: string;
+  emblem: string;
+  name: string;
+  plan: string;
+  area: matchesArea;
+  competition: matchesCompetition;
+  id: number;
+  utcDate: string;
+  status: string;
+  matchday?: number;
+  homeTeam?: matchesHomeTeam;
+  awayTeam?: matchesAwayTeam;
+  score?: scores;
+};
+
+export type newsType = {
+  title: string,
+  url: string,
+  urlToImage?: string,
+}
